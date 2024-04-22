ba1ba262678d98ad9d8ca46d42e8cd1d30b5939c
PARK9898 <wjddjs21@naver.com>
2024-04-22 13:51:56 +0900

    implement main entry point
    실행될 메인 작성 & 정렬 알고리즘 사용

--- java-oop/src/main/java/Main.java
+++ java-oop/src/main/java/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+import logic.BubbleSort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BubbleSort<String> sort = new BubbleSort<>();
+        System.out.println("[result]" + sort.sort(Arrays.asList(args)));
     }
 }
